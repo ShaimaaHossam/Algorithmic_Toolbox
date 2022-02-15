d4962c7d49d966ba2ef0e9ba34dbae183522df39
ShaimaaHossam <40139825+ShaimaaHossam@users.noreply.github.com>
2022-02-15 14:17:25 +0200

    Update change.cpp

--- week3/change.cpp
+++ week3/change.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int get_change(int m) {
-    int i, j, k,n = 0;
+    int i, j, k;
     i = (int) m/10;
     j = (int)((m%10)/5);
     k = m - i*10 - j * 5;

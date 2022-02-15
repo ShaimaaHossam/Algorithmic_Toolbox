82ea361cbeb61a44f8a1606849df29e23e5fa1ef
ShaimaaHossam <40139825+ShaimaaHossam@users.noreply.github.com>
2022-02-15 14:11:57 +0200

    Create change.cpp

---
+++ week3/change.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int get_change(int m) {
+    int i, j, k,n = 0;
+    i = (int) m/10;
+    j = (int)((m%10)/5);
+    k = m - i*10 - j * 5;
+  return i+j+k;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
